bf12faa24321b8c8b285187e47355cb9df667d83
Vicente Woinilowicz <139511901+woinivincent@users.noreply.github.com>
2024-03-13 17:03:07 -0300

    pokemon styled game made with html canvas & vanilla js

---
+++ casiPokemon/Data/attacks.js
@@ -0,0 +1,16 @@
+const attacks = {
+    Tackle: {
+        name: 'Tackle',
+        damage: 10,
+        type: 'Normal',
+        color:'black'
+    },
+    Fireball: {
+        name: 'Fireball',
+        damage: 25,
+        type: 'Fire',
+        color:'red'
+    }
+
+
+}
